2c95338abeb01d414cb1c8668837c648aefcede7
Markov Artem <drownbes@gmail.com>
2014-09-09 00:26:30 +0600

    clean correctly sass-cache

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function (grunt) {
 
 		clean: {
 			src: {
-				src: ['<%= config.dist %>/*']
+				src: ['<%= config.dist %>/*', '<%= config.dist %>/../.sass-cache']
 			}
 		},
 
